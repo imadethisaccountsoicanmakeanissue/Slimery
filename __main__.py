7155d71ba01bb289c861b4b4a3520b95ad22970d
Marc Thompson <75588579+imadethisaccountsoicanmakeanissue@users.noreply.github.com>
2023-07-20 16:42:07 -0500

    Something i forgor

--- __main__.py
+++ __main__.py
@@ -1,10 +1,11 @@
 import pygame
-  
+
 pygame.init()
   
 # CREATING CANVAS
 canvas = pygame.display.set_mode((500, 500))
 icon = pygame.image.load("icon.png")
+
 pygame.display.set_icon(icon)
   
 # TITLE OF CANVAS
